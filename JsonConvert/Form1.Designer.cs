66878725eaedfe444716c34787e87631fa9ebe88
xxc <82869@runjian.com>
2021-09-16 19:35:27 +0800

    fisrt commit

---
+++ JsonConvert/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace JsonConvertTO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_startConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_selectFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_startConvert
+            // 
+            this.btn_startConvert.Location = new System.Drawing.Point(693, 44);
+            this.btn_startConvert.Name = "btn_startConvert";
+            this.btn_startConvert.Size = new System.Drawing.Size(75, 64);
+            this.btn_startConvert.TabIndex = 0;
+            this.btn_startConvert.Text = "开始转换";
+            this.btn_startConvert.UseVisualStyleBackColor = true;
+            this.btn_startConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择文件：";
+            // 
+            // txt_selectFilePath
+            // 
+            this.txt_selectFilePath.Location = new System.Drawing.Point(95, 44);
+            this.txt_selectFilePath.Name = "txt_selectFilePath";
+            this.txt_selectFilePath.ReadOnly = true;
+            this.txt_selectFilePath.Size = new System.Drawing.Size(468, 21);
+            this.txt_selectFilePath.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(577, 42);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(93, 23);
+            this.btn_select.TabIndex = 3;
+            this.btn_select.Text = "选择json文件";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "存放目录：";
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(95, 88);
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ReadOnly = true;
+            this.txt_output.Size = new System.Drawing.Size(468, 21);
+            this.txt_output.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "选择存放路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "执行记录：";
+            // 
+            // txt_log
+            // 
+            this.txt_log.Location = new System.Drawing.Point(95, 132);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ReadOnly = true;
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_log.Size = new System.Drawing.Size(468, 41);
+            this.txt_log.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 185);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.txt_selectFilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_startConvert);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "json文件转换";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_startConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_selectFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_log;
+    }
+}
+
---
+++ JsonConvert/Form1.cs
@@ -0,0 +1,157 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonConvertTO
+{
+    public partial class Form1 : Form
+    {
+        private string fileName;
+        private string outputFileName;
+        private IWorkbook workbook = null;
+        private FileStream fs = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var stream = new StreamReader(fileName);
+                var str = stream.ReadToEnd();
+
+                var json = JsonConvert.DeserializeObject<List<JObject>>(str);
+
+                var data = GenerateData(json);
+                DataTableToExcel(data, "sheet", true);
+            }
+            catch (Exception ex)
+            {
+                txt_log.AppendText(ex.Message+ "\r\n");
+            }
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            var open = openFileDialog1.ShowDialog();
+            if (open == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                txt_selectFilePath.Text = fileName;
+                var fName = Path.GetFileName(fileName);
+                var lastIndex = fName.LastIndexOf(".");
+                outputFileName = fName.Substring(0, lastIndex) + ".xls";
+            }
+        }
+        public DataTable GenerateData(List<JObject> keyValues)
+        {
+            txt_log.AppendText("开始生成数据……\r\n");
+            var colnums = new List<string>();
+
+            var fistData = keyValues.First();
+            var pros = fistData.Properties();
+            foreach (var item in pros)
+            {
+                colnums.Add(item.Name);
+            }
+
+            DataTable data = new DataTable();
+
+            for (int i = 0; i < colnums.Count; ++i)
+            {
+                data.Columns.Add(colnums[i], typeof(string));
+            }
+
+            foreach (var item in keyValues)
+            {
+                var pros2 = item.Properties().ToArray();
+                DataRow row = data.NewRow();
+                for (int y = 0; y < colnums.Count; y++)
+                {
+                    row[colnums[y]] = pros2[y].Value.ToString();
+                }
+                data.Rows.Add(row);
+            }
+            return data;
+        }
+        public int DataTableToExcel(DataTable data, string sheetName, bool isColumnWritten)
+        {
+            txt_log.AppendText("开始导出……\r\n");
+            var _outputFileName = Path.Combine(txt_output.Text, outputFileName);
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            ISheet sheet = null;
+
+            fs = new FileStream(_outputFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            if (_outputFileName.IndexOf(".xlsx") > 0) // 2007版本
+                workbook = new XSSFWorkbook();
+            else if (_outputFileName.IndexOf(".xls") > 0) // 2003版本
+                workbook = new HSSFWorkbook();
+
+
+            if (workbook != null)
+            {
+                sheet = workbook.CreateSheet(sheetName);
+            }
+            else
+            {
+                return -1;
+            }
+
+            if (isColumnWritten == true) //写入DataTable的列名
+            {
+                IRow row = sheet.CreateRow(0);
+                for (j = 0; j < data.Columns.Count; ++j)
+                {
+                    row.CreateCell(j).SetCellValue(data.Columns[j].ColumnName);
+                }
+                count = 1;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            for (i = 0; i < data.Rows.Count; ++i)
+            {
+                IRow row = sheet.CreateRow(count);
+                for (j = 0; j < data.Columns.Count; ++j)
+                {
+                    row.CreateCell(j).SetCellValue(data.Rows[i][j].ToString());
+                }
+                ++count;
+            }
+            workbook.Write(fs); //写入到excel
+            workbook.Close();
+            fs.Flush();
+            fs.Close();
+            txt_log.AppendText($"共导出{count}条！\r\n");
+            return count;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var open = folderBrowserDialog1.ShowDialog();
+            if (open == DialogResult.OK)
+            {
+                txt_output.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
